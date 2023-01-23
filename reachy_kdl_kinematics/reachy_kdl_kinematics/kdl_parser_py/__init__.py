cb30f7781a489798ba6aee0945d161e453bda5ae
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-01-23 17:55:03 +0100

    Setup kinematics package with urdf parser.

---
+++ reachy_kdl_kinematics/reachy_kdl_kinematics/kdl_parser_py/__init__.py
@@ -0,0 +1,8 @@
+"""kdl_parser_py.
+
+The urdf_parser_py package was copied from https://github.com/ros/kdl_parser.
+All credits from https://github.com/ros/kdl_parser developpers.
+
+It was adapted to run within reachy_kinematics.
+We use it as an internal dependencies instead of an external one to simplify the installation.
+"""
