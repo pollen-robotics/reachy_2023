63fb6f145a1ee7b86547f88c473fa2f286ceba6b
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2023-01-23 18:12:32 +0100

    Add launch files.

---
+++ reachy_sdk_server/launch/reachy_camera_sdk_server.launch.py
@@ -0,0 +1,16 @@
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='reachy_sdk_server',
+            executable='reachy_sdk_server',
+        ),
+        Node(
+            package='reachy_sdk_server',
+            executable='camera_server',
+        ),
+    ])
