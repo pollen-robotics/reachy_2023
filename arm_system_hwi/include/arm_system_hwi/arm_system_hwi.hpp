b0a3d9fad65dcaa3c4507570c54aadb83e4c3349
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-01-24 12:31:30 +0100

    Update arm_system_hwi.hpp

--- arm_system_hwi/include/arm_system_hwi/arm_system_hwi.hpp
+++ arm_system_hwi/include/arm_system_hwi/arm_system_hwi.hpp
@@ -26,12 +26,8 @@ public:
     CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
     CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
 
-    // hardware_interface::return_type read() override;
-    // hardware_interface::return_type write() override;
     hardware_interface::return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
     hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
-  
-  
 
 private:
 // MX
