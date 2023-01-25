55f17ef17c3afd20712718d24271d4e85a1ac063
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-01-25 14:39:15 +0100

    Update type_conversion.py

--- reachy_sdk_server/reachy_sdk_server/type_conversion.py
+++ reachy_sdk_server/reachy_sdk_server/type_conversion.py
@@ -22,4 +22,21 @@ def pb_matrix_from_ros_pose(pose: Pose) -> Matrix4x4:
     M[:3, :3] = Rotation.from_quat(q).as_matrix()
 
     return Matrix4x4(data=M.flatten())
-    
+
+
+def ros_pose_from_pb_matrix(m: Matrix4x4) -> Pose:
+    M = np.array(m.data).reshape((4, 4))
+
+    pose = Pose()
+
+    pose.position.x = M[0, 3]
+    pose.position.y = M[1, 3]
+    pose.position.z = M[2, 3]
+
+    q = Rotation.from_matrix(M[:3, :3]).as_quat()
+    pose.orientation.x = q[0]
+    pose.orientation.y = q[1]
+    pose.orientation.z = q[2]
+    pose.orientation.w = q[3]
+
+    return pose
