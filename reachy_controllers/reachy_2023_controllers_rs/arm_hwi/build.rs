c90289d9a2adae330fe71053ce6c275a2c1aeb9a
Clément Dussieux <clement.dussieux@pollen-robotics.com>
2023-02-09 12:43:22 +0100

    fix .h generation

--- reachy_controllers/reachy_2023_controllers_rs/arm_hwi/build.rs
+++ reachy_controllers/reachy_2023_controllers_rs/arm_hwi/build.rs
@@ -9,5 +9,5 @@ fn main() {
         .with_crate(crate_dir)
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file("head_hwi.h");
+        .write_to_file("arm_hwi.h");
 }
