d05fff9843cdad5b67d94fc86410f1cb6141d6f1
Steve Nguyen <steve.nguyen@pollen-robotics.com>
2023-02-14 17:51:04 +0100

    kinda fix this use_sim_time the ugly way

--- reachy_moveit_config/launch/spawn_controllers.launch.py
+++ reachy_moveit_config/launch/spawn_controllers.launch.py
@@ -1,5 +1,43 @@
 from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_spawn_controllers_launch
+# from moveit_configs_utils.launches import generate_spawn_controllers_launch
+
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+)
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node, SetUseSimTime
+from launch_ros.parameter_descriptions import ParameterValue
+
+from srdfdom.srdf import SRDF
+
+from moveit_configs_utils.launch_utils import (
+    add_debuggable_node,
+    DeclareBooleanLaunchArg,
+)
+
+
+def generate_spawn_controllers_launch(moveit_config):
+    SetUseSimTime(True)
+    controller_names = moveit_config.trajectory_execution.get(
+        "moveit_simple_controller_manager", {}
+    ).get("controller_names", [])
+    ld = LaunchDescription()
+    for controller in controller_names + ["joint_state_broadcaster"]:
+        ld.add_action(
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[controller, '-c', '/controller_manager'],
+                output="screen",
+                parameters=[{'use_sim_time': True}]
+            )
+        )
+    return ld
 
 
 def generate_launch_description():
