d1b8b9dfcb0354d673d24624b2b8b96e5ae27f89
Clément Dussieux <clement.dussieux@pollen-robotics.com>
2023-03-23 14:26:09 +0100

    moved source env

--- reachy_sdk_server/launch.bash
+++ reachy_sdk_server/launch.bash
@@ -1,11 +1,5 @@
-source /opt/ros/humble/setup.bash
-source $HOME/reachy_ws/install/setup.bash
-source /usr/share/gazebo/setup.bash
-source /usr/share/colcon_cd/function/colcon_cd.sh
-export _colcon_cd_root=~/ros2_install
-export LC_NUMERIC="en_US.UTF-8"
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+# Env setup
+source /home/reachy/reachy_ws/src/reachy_2023/reachy_utils/.reachy.bashrc
 
-source "$HOME/.cargo/env"
 
 ros2 launch reachy_bringup reachy.launch.py start_sdk_server:=true
