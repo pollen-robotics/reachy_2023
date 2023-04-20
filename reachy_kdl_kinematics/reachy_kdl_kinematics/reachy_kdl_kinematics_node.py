ec500b2f2e3aaad21de2f5f78daaa1381aa46e21
RemiFabre <remifabre1800@gmail.com>
2023-04-20 23:25:11 +0200

    reachy placo integration wip

--- reachy_kdl_kinematics/reachy_kdl_kinematics/reachy_kdl_kinematics_node.py
+++ reachy_kdl_kinematics/reachy_kdl_kinematics/reachy_kdl_kinematics_node.py
@@ -26,11 +26,12 @@ from .kdl_kinematics import (
     inverse_kinematics,
     ros_pose_to_matrix,
 )
+import ik_reachy_placo
 
 
 class ReachyKdlKinematics(LifecycleNode):
     def __init__(self):
-        super().__init__('reachy_kdl_kinematics_node')
+        super().__init__("reachy_kdl_kinematics_node")
         self.logger = self.get_logger()
 
         self.urdf = self.retrieve_urdf()
@@ -40,7 +41,7 @@ class ReachyKdlKinematics(LifecycleNode):
         self._current_pos = {}
         self.joint_state_sub = self.create_subscription(
             msg_type=JointState,
-            topic='/joint_states',
+            topic="/joint_states",
             qos_profile=5,
             callback=self.on_joint_state,
         )
@@ -53,10 +54,23 @@ class ReachyKdlKinematics(LifecycleNode):
         self.averaged_pose = {}
         self.max_joint_vel = {}
 
-        for prefix in ('l', 'r'):
-            arm = f'{prefix}_arm'
+        # Setuping the QP solver
+        # TODO Placo will be updated soon, this workaround is temporay
+        # https://github.com/Rhoban/placo/issues/1
+        urdf_path = "/tmp/reachy.urdf"
+        tmp_file = open(urdf_path, "w+")
+        tmp_file.write(self.urdf)
+        tmp_file.close()
+        self.ik_reachy_placo = ik_reachy_placo.IKReachyQP(viewer_on=True)
+        self.ik_reachy_placo.setup(urdf_path)
+        self.ik_reachy_placo.create_tasks()
 
-            chain, fk_solver, ik_solver = generate_solver(self.urdf, 'torso', f'{prefix}_arm_tip')
+        for prefix in ("l", "r"):
+            arm = f"{prefix}_arm"
+
+            chain, fk_solver, ik_solver = generate_solver(
+                self.urdf, "torso", f"{prefix}_arm_tip"
+            )
 
             # We automatically loads the kinematics corresponding to the config
             if chain.getNrOfJoints():
@@ -65,7 +79,7 @@ class ReachyKdlKinematics(LifecycleNode):
                 # Create forward kinematics service
                 self.fk_srv[arm] = self.create_service(
                     srv_type=GetForwardKinematics,
-                    srv_name=f'/{arm}/forward_kinematics',
+                    srv_name=f"/{arm}/forward_kinematics",
                     callback=partial(self.forward_kinematics_srv, name=arm),
                 )
                 self.logger.info(f'Adding service "{self.fk_srv[arm].srv_name}"...')
@@ -73,7 +87,7 @@ class ReachyKdlKinematics(LifecycleNode):
                 # Create inverse kinematics service
                 self.ik_srv[arm] = self.create_service(
                     srv_type=GetInverseKinematics,
-                    srv_name=f'/{arm}/inverse_kinematics',
+                    srv_name=f"/{arm}/inverse_kinematics",
                     callback=partial(self.inverse_kinematics_srv, name=arm),
                 )
                 self.logger.info(f'Adding service "{self.ik_srv[arm].srv_name}"...')
@@ -81,13 +95,13 @@ class ReachyKdlKinematics(LifecycleNode):
                 # Create cartesian control pub/subscription
                 forward_position_pub = self.create_publisher(
                     msg_type=Float64MultiArray,
-                    topic=f'/{arm}_forward_position_controller/commands',
+                    topic=f"/{arm}_forward_position_controller/commands",
                     qos_profile=5,
                 )
 
                 self.target_sub[arm] = self.create_subscription(
                     msg_type=PoseStamped,
-                    topic=f'/{arm}/target_pose',
+                    topic=f"/{arm}/target_pose",
                     qos_profile=5,
                     callback=partial(
                         self.on_target_pose,
@@ -97,11 +111,13 @@ class ReachyKdlKinematics(LifecycleNode):
                         forward_publisher=forward_position_pub,
                     ),
                 )
-                self.logger.info(f'Adding subscription on "{self.target_sub[arm].topic}"...')
+                self.logger.info(
+                    f'Adding subscription on "{self.target_sub[arm].topic}"...'
+                )
 
                 self.averaged_target_sub[arm] = self.create_subscription(
                     msg_type=PoseStamped,
-                    topic=f'/{arm}/averaged_target_pose',
+                    topic=f"/{arm}/averaged_target_pose",
                     qos_profile=5,
                     callback=partial(
                         self.on_averaged_target_pose,
@@ -113,87 +129,96 @@ class ReachyKdlKinematics(LifecycleNode):
                 )
                 self.averaged_pose[arm] = PoseAverager(window_length=1)
                 self.max_joint_vel[arm] = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
-                self.logger.info(f'Adding subscription on "{self.target_sub[arm].topic}"...')
+                self.logger.info(
+                    f'Adding subscription on "{self.target_sub[arm].topic}"...'
+                )
 
                 self.chain[arm] = chain
                 self.fk_solver[arm] = fk_solver
                 self.ik_solver[arm] = ik_solver
 
         # Kinematics for the head
-        chain, fk_solver, ik_solver = generate_solver(self.urdf, 'torso', 'head_tip', L=np.array([1e-6, 1e-6, 1e-6, 1.0, 1.0, 1.0]))  # L weight matrix to considere only the orientation
+        chain, fk_solver, ik_solver = generate_solver(
+            self.urdf,
+            "torso",
+            "head_tip",
+            L=np.array([1e-6, 1e-6, 1e-6, 1.0, 1.0, 1.0]),
+        )  # L weight matrix to considere only the orientation
 
-        # We automatically loads the kinematics corresponding to the config
+        # We automatically load the kinematics corresponding to the config
         if chain.getNrOfJoints():
-            self.logger.info(f'Found kinematics chain for head!')
+            self.logger.info(f"Found kinematics chain for head!")
 
             # Create forward kinematics service
             srv = self.create_service(
                 srv_type=GetForwardKinematics,
-                srv_name='/head/forward_kinematics',
-                callback=partial(self.forward_kinematics_srv, name='head'),
+                srv_name="/head/forward_kinematics",
+                callback=partial(self.forward_kinematics_srv, name="head"),
             )
-            self.fk_srv['head'] = srv
+            self.fk_srv["head"] = srv
             self.logger.info(f'Adding service "{srv.srv_name}"...')
 
             # Create inverse kinematics service
             srv = self.create_service(
                 srv_type=GetInverseKinematics,
-                srv_name='/head/inverse_kinematics',
-                callback=partial(self.inverse_kinematics_srv, name='head'),
+                srv_name="/head/inverse_kinematics",
+                callback=partial(self.inverse_kinematics_srv, name="head"),
             )
-            self.ik_srv['head'] = srv
+            self.ik_srv["head"] = srv
             self.logger.info(f'Adding service "{srv.srv_name}"...')
 
             # Create cartesian control subscription
             head_forward_position_pub = self.create_publisher(
                 msg_type=Float64MultiArray,
-                topic='/neck_forward_position_controller/commands',  # need
+                topic="/neck_forward_position_controller/commands",  # need
                 qos_profile=5,
             )
 
             sub = self.create_subscription(
                 msg_type=PoseStamped,
-                topic='/head/target_pose',
+                topic="/head/target_pose",
                 qos_profile=5,
                 callback=partial(
                     self.on_target_pose,
-                    name='head',
+                    name="head",
                     # head straight
                     q0=[0, 0, 0],
                     forward_publisher=head_forward_position_pub,
                 ),
             )
-            self.target_sub['head'] = sub
+            self.target_sub["head"] = sub
             self.logger.info(f'Adding subscription on "{sub.topic}"...')
 
             sub = self.create_subscription(
                 msg_type=PoseStamped,
-                topic='/head/averaged_target_pose',
+                topic="/head/averaged_target_pose",
                 qos_profile=5,
                 callback=partial(
                     self.on_averaged_target_pose,
-                    name='head',
+                    name="head",
                     # head straight
                     q0=[0, 0, 0],
                     forward_publisher=head_forward_position_pub,
                 ),
             )
-            self.averaged_target_sub['head'] = sub
-            self.averaged_pose['head'] = PoseAverager(window_length=1)
+            self.averaged_target_sub["head"] = sub
+            self.averaged_pose["head"] = PoseAverager(window_length=1)
 
-            self.max_joint_vel['head'] = np.array([0.1, 0.1, 0.1])
+            self.max_joint_vel["head"] = np.array([0.1, 0.1, 0.1])
             self.logger.info(f'Adding subscription on "{sub.topic}"...')
 
-            self.chain['head'] = chain
-            self.fk_solver['head'] = fk_solver
-            self.ik_solver['head'] = ik_solver
+            self.chain["head"] = chain
+            self.fk_solver["head"] = fk_solver
+            self.ik_solver["head"] = ik_solver
 
-        self.logger.info(f'Kinematics node ready!')
+        self.logger.info(f"Kinematics node ready!")
         self.trigger_configure()
 
     def on_configure(self, state: State) -> TransitionCallbackReturn:
         # Dummy state to minimize impact on current behavior
-        self.logger.info("Configuring state has been called, going into inactive to release event trigger")
+        self.logger.info(
+            "Configuring state has been called, going into inactive to release event trigger"
+        )
         return TransitionCallbackReturn.SUCCESS
 
     def forward_kinematics_srv(
@@ -203,7 +228,9 @@ class ReachyKdlKinematics(LifecycleNode):
         name,
     ) -> GetForwardKinematics.Response:
         try:
-            joint_position = self.check_position(request.joint_position, self.chain[name])
+            joint_position = self.check_position(
+                request.joint_position, self.chain[name]
+            )
         except KeyError:
             response.success = False
             return response
@@ -237,15 +264,22 @@ class ReachyKdlKinematics(LifecycleNode):
         response: GetInverseKinematics.Response,
         name,
     ) -> GetInverseKinematics.Response:
-
         M = ros_pose_to_matrix(request.pose)
         q0 = request.q0.position
 
-        error, sol = inverse_kinematics(
-            self.ik_solver[name],
+        # IK using KDL
+        # error, sol = inverse_kinematics(
+        #     self.ik_solver[name],
+        #     q0=q0,
+        #     target_pose=M,
+        #     nb_joints=self.chain[name].getNrOfJoints(),
+        # )
+
+        # IK using Placo
+        sol, errors = self.ik_reachy_placo.ik_continuous(
+            M,
+            arm_name=name,
             q0=q0,
-            target_pose=M,
-            nb_joints=self.chain[name].getNrOfJoints(),
         )
 
         # TODO: use error
@@ -258,11 +292,19 @@ class ReachyKdlKinematics(LifecycleNode):
     def on_target_pose(self, msg: PoseStamped, name, q0, forward_publisher):
         M = ros_pose_to_matrix(msg.pose)
 
-        error, sol = inverse_kinematics(
-            self.ik_solver[name],
+        # IK using KDL
+        # error, sol = inverse_kinematics(
+        #     self.ik_solver[name],
+        #     q0=q0,
+        #     target_pose=M,
+        #     nb_joints=self.chain[name].getNrOfJoints(),
+        # )
+
+        # IK using Placo
+        sol, errors = self.ik_reachy_placo.ik_continuous(
+            M,
+            arm_name=name,
             q0=q0,
-            target_pose=M,
-            nb_joints=self.chain[name].getNrOfJoints(),
         )
 
         # TODO: check error
@@ -278,11 +320,19 @@ class ReachyKdlKinematics(LifecycleNode):
 
         M = ros_pose_to_matrix(avg_pose)
 
-        error, sol = inverse_kinematics(
-            self.ik_solver[name],
+        # IK using KDL
+        # error, sol = inverse_kinematics(
+        #     self.ik_solver[name],
+        #     q0=q0,
+        #     target_pose=M,
+        #     nb_joints=self.chain[name].getNrOfJoints(),
+        # )
+
+        # IK using Placo
+        sol, errors = self.ik_reachy_placo.ik_continuous(
+            M,
+            arm_name=name,
             q0=q0,
-            target_pose=M,
-            nb_joints=self.chain[name].getNrOfJoints(),
         )
 
         # TODO: check error
@@ -305,7 +355,7 @@ class ReachyKdlKinematics(LifecycleNode):
 
     def wait_for_joint_state(self):
         while not self.joint_state_ready.is_set():
-            self.logger.info('Waiting for /joint_states...')
+            self.logger.info("Waiting for /joint_states...")
             rclpy.spin_once(self)
 
     def on_joint_state(self, msg: JointState):
@@ -326,17 +376,18 @@ class ReachyKdlKinematics(LifecycleNode):
             self.urdf = msg.data
 
         self.create_subscription(
-            msg_type=String, topic='/robot_description',
+            msg_type=String,
+            topic="/robot_description",
             qos_profile=qos_profile,
             callback=urdf_received,
         )
         rclpy.spin_once(self, timeout_sec=timeout_sec)
 
         if self.urdf is None:
-            self.logger.error('Could not retrieve the URDF!')
-            raise EnvironmentError('Could not retrieve the URDF!')
+            self.logger.error("Could not retrieve the URDF!")
+            raise EnvironmentError("Could not retrieve the URDF!")
 
-        self.logger.info('Done!')
+        self.logger.info("Done!")
 
         return self.urdf
 
@@ -346,11 +397,16 @@ class ReachyKdlKinematics(LifecycleNode):
             joints = [pos[j] for j in self.get_chain_joints_name(chain)]
             return joints
         except KeyError:
-            self.logger.warning(f'Incorrect joints found ({js.name} vs {self.get_chain_joints_name(chain)})')
+            self.logger.warning(
+                f"Incorrect joints found ({js.name} vs {self.get_chain_joints_name(chain)})"
+            )
             raise
 
     def get_chain_joints_name(self, chain):
-        return [chain.getSegment(i).getJoint().getName() for i in range(chain.getNrOfJoints())]
+        return [
+            chain.getSegment(i).getJoint().getName()
+            for i in range(chain.getNrOfJoints())
+        ]
 
 
 def main():
@@ -359,5 +415,5 @@ def main():
     rclpy.spin(node)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
