f3fb3049f0099de21407285357a13ea93f4079b4
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2023-05-02 16:28:03 +0200

    Use reachy_utils to get zuuu_version.

--- mobile_base_controller/zuuu_hal/zuuu_hal/__init__.py
+++
@@ -1,7 +0,0 @@
-def identify_zuuu_model():
-    import yaml
-    import os
-    config_file = os.path.expanduser('~/.reachy.yaml')
-    with open(config_file) as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-        return config['zuuu_version']
