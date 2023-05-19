24072c7e933083904ed7831f7eb9b7fee06a1601
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2023-05-19 16:39:39 +0200

    Update setup.py

--- reachy_controllers/fans_controller/setup.py
+++ reachy_controllers/fans_controller/setup.py
@@ -4,7 +4,7 @@ package_name = 'fans_controller'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='simsim',
-    maintainer_email='32677536+simheo@users.noreply.github.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Pollen Robotics',
+    maintainer_email='contact@pollen-robotics.com',
+    description='ROS2 Humble controller for Reachy\'s fans.',
+    license='Apache License, Version 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
