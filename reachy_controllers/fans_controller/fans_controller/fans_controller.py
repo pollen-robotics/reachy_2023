be4dec12212b651b7c19df15e6112cbb7b8b76d1
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2023-05-19 16:15:28 +0200

    Update fans controller main node.
    - Only publish fan command when needed,
    - Set the fans state correctly when starting the node, in case some fans were already turned from the dashboard or somewhere else.

--- reachy_controllers/fans_controller/fans_controller/fans_controller.py
+++ reachy_controllers/fans_controller/fans_controller/fans_controller.py
@@ -32,13 +32,24 @@ class FansController(Node):
     def __init__(self, node_name):
         # Call the constructor of the parent class (Node).
         super().__init__(node_name)
+        self._logger = self.get_logger()
+
         robot_parts = robot_config_to_parts[get_reachy_model()]
         
-        # Define the fan trigger temperature from get_fan_trigger_temperature.
+        # Define the fan trigger temperature from get_fan_trigger_temperature. 
         self._fan_trigger_temperature = get_fan_trigger_temperature()
+        if self._fan_trigger_temperature == -1:
+            # print an error message if the fan trigger temperature is not defined.
+            # and exit the program.
+            self.get_logger().error('Fan trigger temperature is not defined.')
+            exit()
+
+        # print self._fan_trigger_temperature
+        self._logger.info(f'Fan trigger temperature is {self._fan_trigger_temperature}°C.')
 
         # Define a dictionnary to store the fan state.
         self._fan_states = {}
+        self._fan_states_to_publish = []
         self._motor_temperatures = {}
 
         for part in robot_parts:
@@ -60,15 +71,19 @@ class FansController(Node):
         # with queue size 5.
         self._joint_state_pub = self.create_publisher(
             DynamicJointState,
-            'dynamic_joint_states',
+            'dynamic_joint_commands',
             5)
-        
+
+        self._logger.info('Fans controller node started.')
+
+        self._handle_first_message()
+
         # Create a timer with a period of 1 second and the callback function 'self._on_timer'.
         self._timer = self.create_timer(1, self._on_timer)
 
         # Define a logger attribute to print the fan states.
-        self._logger = self.get_logger()
 
+        # Log a message to indicate that the node is started.
 
     # Define the callback function '_on_joint_state'.
     def _on_joint_state(self, msg):
@@ -87,25 +102,59 @@ class FansController(Node):
             # Example: 'l_shoulder_pitch' -> 'l_shoulder'
             motor = motor.split('_pitch')[0]
 
-            if temperature >= self._fan_trigger_temperature:
+            if temperature >= self._fan_trigger_temperature and not self._fan_states[f'{motor}_fan']:
                 # Set the fan state to True.
+                # Log the fan name and the motor temperature.
+                # self._logger.info(f'{motor}_fan: {temperature}')
+
                 self._fan_states[f'{motor}_fan'] = True
+                self._fan_states_to_publish.append(f'{motor}_fan')
             
             # else if temperature is below the fan trigger temperature -2°C, set the fan state to False.
-            elif temperature < self._fan_trigger_temperature - 2:
+            elif temperature < self._fan_trigger_temperature - 2 and self._fan_states[f'{motor}_fan']:
                 # Get the fan name from the motor name by replacing 'pitch' with 'fan'.
                 # Example: 'l_shoulder_pitch' -> 'l_shoulder_fan'
                 self._fan_states[f'{motor}_fan'] = False
-
-        # Print the fan states and the motor temperatures.
-        self._logger.info(f'Fans state: {self._fan_states}')
-        self._logger.info(f'Motors temperature: {self._motor_temperatures}')
+                self._fan_states_to_publish.append(f'{motor}_fan')
 
         # Create a DynamicJointState msg.
+        if self._fan_states_to_publish:
+            msg = DynamicJointState()
+            msg.joint_names = self._fan_states_to_publish
+            # Iterate over each fan state and set the interface values.
+            # Example: {'l_shoulder_fan': True, 'r_shoulder_fan': False} -> [InterfaceValue(interface_names=['state'], values=[1.0]), InterfaceValue(interface_names=['state'], values=[0.0])]
+            msg.interface_values = [
+                InterfaceValue(
+                    interface_names=['state'],
+                    values=[float(self._fan_states[fan])],
+                )
+                for fan in self._fan_states_to_publish
+            ]
+
+            # Publish the msg.
+            self._joint_state_pub.publish(msg)
+
+
+            for fan in self._fan_states_to_publish:
+                state = 'ON' if self._fan_states[fan] else 'OFF'
+                self._logger.info(f'Turning {state} {fan}.')
+
+            self._fan_states_to_publish = []
+            # Print the msg.
+            # self._logger.info(f'Publishing fan states: {msg}')
+
+    def _handle_first_message(self):
+        for motor, temperature in self._motor_temperatures.items():
+            motor = motor.split('_pitch')[0]
+
+            if temperature >= self._fan_trigger_temperature:
+                self._fan_states[f'{motor}_fan'] = True
+
+            elif temperature < self._fan_trigger_temperature - 2 and self._fan_states[f'{motor}_fan']:
+                self._fan_states[f'{motor}_fan'] = False
+
         msg = DynamicJointState()
-        msg.joint_names = self._fan_states.keys()
-        # Iterate over each fan state and set the interface values.
-        # Example: {'l_shoulder_fan': True, 'r_shoulder_fan': False} -> [InterfaceValue(interface_names=['state'], values=[1.0]), InterfaceValue(interface_names=['state'], values=[0.0])]
+        msg.joint_names = list(self._fan_states.keys())
         msg.interface_values = [
             InterfaceValue(
                 interface_names=['state'],
@@ -113,9 +162,11 @@ class FansController(Node):
             )
             for state in self._fan_states.values()
         ]
-
-        # Publish the msg.
         self._joint_state_pub.publish(msg)
+        self._logger.info(f'Sent initial fan command:')
+        for (fan, state) in self._fan_states.items():
+            state = 'ON' if state else 'OFF'
+            self._logger.info(f'    {fan} is {state}')
 
 
 # Create the main function to initialize the node and call the class.
