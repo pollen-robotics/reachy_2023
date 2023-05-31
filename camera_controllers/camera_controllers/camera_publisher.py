7b28827d962dc2bfde931d13e45110750493f5c9
apirrone <awanegenebayo@gmail.com>
2023-05-31 16:23:42 +0200

    working camera_publisher.py for luxonis

--- camera_controllers/camera_controllers/camera_publisher.py
+++ camera_controllers/camera_controllers/camera_publisher.py
@@ -5,14 +5,10 @@ Camera Node.
 
 Supported resolutions are: (1920,1080) at 15FPS, (1280,720) at 30FPS, (640,480) at 30FPS. <-- TODO update for luxonis FFC if needed
 """
-from functools import partial
-from threading import Thread
 from typing import Dict
 from subprocess import Popen, PIPE
 from ffc_wrapper import FFCWrapper
-import cv2
 from cv_bridge import CvBridge
-import numpy as np
 
 import rclpy
 from rclpy.node import Node
@@ -33,12 +29,12 @@ class CameraPublisher(Node):
         self.logger = self.get_logger()
         self.clock = self.get_clock()
 
-        self.ffcw = FFCWrapper("/home/reachy/dev/Reachy_stereo/luxonis/MY_MODULAR_CONFIG.json")
+        self.ffcw = FFCWrapper("/home/reachy/dev/Reachy_stereo/luxonis/MY_MODULAR_CONFIG.json", resize=True, imsize=(888, 500))
 
-        self.camera_publisher_left = self.create_publisher(CompressedImage, 'left_image', 1)
+        self.camera_publisher_left = self.create_publisher(CompressedImage, 'left_image/image_raw/compressed', 1)
         self.logger.info(f'Launching "{self.camera_publisher_left.topic_name}" publisher.')
 
-        self.camera_publisher_right = self.create_publisher(CompressedImage, 'right_image', 1)
+        self.camera_publisher_right = self.create_publisher(CompressedImage, 'right_image/image_raw/compressed', 1)
         self.logger.info(f'Launching "{self.camera_publisher_right.topic_name}" publisher.')
 
         self.publisher = {
@@ -50,45 +46,48 @@ class CameraPublisher(Node):
 
         self.bridge = CvBridge()
 
+        self.last_images = {"left": None, "right": None}
+
         def publisher():
-            self.logger.info('Left and Right cameras ready to publish!')
-            while True:
-                imgList = self.ffcw.get_sync_images(rectify=True, invert_h=True, invert_v=True)
-                if imgList is not None:
-                    self.logger.info('PUBLISHING')
-                    leftIm = imgList["left"]
-                    rightIm = imgList["right"]
-                    
-                    leftIm = cv2.resize(leftIm, (0, 0), fx=0.5, fy=0.5)
-                    rightIm = cv2.resize(rightIm, (0, 0), fx=0.5, fy=0.5)
-
-                    # im = np.random.rand(1920, 1080, 3)*255
-
-                    # leftIm = self.bridge.cv2_to_compressed_imgmsg(im, "jpeg")
-                    # rightIm = self.bridge.cv2_to_compressed_imgmsg(im, "jpeg")
-
-                    leftIm = self.bridge.cv2_to_compressed_imgmsg(leftIm, "jpeg")
-                    rightIm = self.bridge.cv2_to_compressed_imgmsg(rightIm, "jpeg")
-
-                    self.publish_img("left", leftIm)
-                    self.publish_img("right", rightIm)
+            if self.last_images is not None:
+                if self.last_images["left"] is not None:
+                    self.publish_img("left", self.last_images["left"])
+                if self.last_images["right"] is not None:
+                    self.publish_img("right", self.last_images["right"])
 
         for side in ('left', 'right'):
             compr_img = CompressedImage()
             compr_img.format = 'jpeg'
             self.compr_img[side] = compr_img
 
-        t = Thread(target=publisher)
-        t.daemon = True
-        t.start()
+        self.publisher_timer = self.create_timer(
+            timer_period_sec=1/30,
+            callback=publisher,
+        )
+        self.get_images_timer = self.create_timer(
+            timer_period_sec=1/30,
+            callback=self.get_images,
+        )
 
         self.logger.info('Node ready!')
 
+    def get_images(self):
+        imgList = self.ffcw.get_images(rectify=True)
+
+        if imgList is None:
+            return
+
+        if imgList["left"] is not None:
+            leftIm = imgList["left"]
+            leftIm = self.bridge.cv2_to_compressed_imgmsg(leftIm, "jpg")
+            self.last_images["left"] = leftIm
+        if imgList["right"] is not None:
+            rightIm = imgList["right"]
+            rightIm = self.bridge.cv2_to_compressed_imgmsg(rightIm, "jpg")
+            self.last_images["right"] = rightIm
+
     def publish_img(self, side: str, frame: bytes) -> None:
         """Read image from the requested side and publishes it."""
-        # compr_img = self.compr_img[side]
-        # compr_img.header.stamp = self.clock.now().to_msg()
-        # compr_img.data = frame
         frame.header.stamp = self.clock.now().to_msg()
         self.publisher[side].publish(frame)
 
